f6f5cf961447569b0a0706066ec8bc018ab1205c
HassanRana009 <iamhassanrana265@gmail.com>
2026-02-17 14:35:19 +0500

    API_BASE_URL fixed

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 // export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://api.yieldscan.io';
 // export const API_BASE_URL = 'https://api.yieldscan.io';
-export const API_BASE_URL = 'http://178.63.197.102:4023/';
+export const API_BASE_URL = 'http://178.63.197.102:4023';
 
 export const CHAIN_NAMES = {
   ETH: 'Ethereum',
