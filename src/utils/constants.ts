943a0e8921c4840ff26d38cf9e3acaf5422a9a30
MuhammadUmerHaider <mumerhaider9@gmail.com>
2026-03-01 17:28:08 +0500

    base url changed

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,7 @@
 // export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://api.yieldscan.io';
-// export const API_BASE_URL = 'https://api.yieldscan.io';
-export const API_BASE_URL = 'http://178.63.197.102:4023';
+export const API_BASE_URL = 'https://api.yieldscan.io';
+// export const API_BASE_URL = 'http://178.63.197.102:4023';
+// export const API_BASE_URL = 'http://localhost:4023';
 
 export const CHAIN_NAMES = {
   ETH: 'Ethereum',
